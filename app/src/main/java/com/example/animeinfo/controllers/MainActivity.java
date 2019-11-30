88c636c4bdd07785f74c9a71592b96a81f85306a
Eric <worlldleric@gmail.com>
2019-11-30 20:42:12 +0100

    Version 5 - Ventana de subir dato creado - Pero no terminado

--- app/src/main/java/com/example/animeinfo/controllers/MainActivity.java
+++ app/src/main/java/com/example/animeinfo/controllers/MainActivity.java
@@ -117,12 +117,27 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
     /** Insertar datos iniciales al RecyclerView **/
     public void llenarAnimes() {
-        listaAnimes.add(new Anime(1,"Tokyo Ghoul", true, R.drawable.tokyoghoul, "En Tokio ocurren varias muertes, cometidas por Ghouls, seres desconocidos que sobreviven a base de carne humana. Un día Ken Kaneki, un joven de 18 años conoce a una chica llamada Rize Kamishiro en una cafetería y la invita a salir. Tras una cita aparentemente normal, Kaneki se ofrece a acompañarla a su casa. Al entrar a un callejón es atacado por ella, quien resulta ser un Ghoul. Durante el ataque, Kaneki es salvado por la caída de unas vigas que matan a Rize antes de que pueda matarlo, pero lo deja terriblemente herido. Debido a la gravedad de sus heridas, recibe un trasplante de órganos por el ghoul atacante (Rize Kamishiro). Así, Kaneki termina convirtiéndose en un ser mitad Ghoul y mitad Humano, de ahora en adelante deberá vivir escondiéndose de los humanos sin tener a quién recurrir. Conoce a unos Ghouls quienes trabajan en la cafetería donde conoció a Rize y se entera que los trabajadores de esa cafetería también resultan ser ghouls que sobreviven a base de cafe, ya que el cafe es el único gusto que comparten los Ghouls y los humanos. Lo demás es rechazado por el estómago de los Ghouls, salvo la carne humana, que han de consumir cada cierto tiempo.Durante el transcurso de la historia Kaneki conocerá a nuevos compañeros y verá el lado oscuro de tanto la humanidad como de los Ghouls"));
-        listaAnimes.add(new Anime(2,"Ataque a los Titanes", false, R.drawable.ataque_a_los_titanes, "La historia comienza con Eren Jaeger quien vive de forma pacífica con su familia, su hermana adoptiva Mikasa Ackerman, y su único amigo Armin Arlert. Estos viven en el «distrito de Shiganshina» (シガンシナ区 Shiganshina-ku?), una de las principales ciudades, ubicadas en la muralla María. Pero todo cambia cuando un titán gigantesco crea una enorme grieta permitiendo la entrada de los titanes a la ciudad, mientras que otro titan fuertemente blindado traspasa la muralla María haciendo de su protección inútil, y provocando una evacuación masiva en toda la población hacia la muralla interna Rose, la segunda de tres murallas concéntricas. Durante el ataque, Eren observa con horror como un titán se come a su madre, mientras que su padre desaparece después de entregarle a Eren la llave del sótano de su casa, no sin antes encomendarle regresar a su hogar algún día. Después de la desaparición de su padre, Eren jura matar a todos los titanes y se alista en el ejército junto con sus amigos.\n" +
+        listaAnimes.add(new Anime(0,"Tokyo Ghoul", true, R.drawable.tokyoghoul, "En Tokio ocurren varias muertes, cometidas por Ghouls, seres desconocidos que sobreviven a base de carne humana. Un día Ken Kaneki, un joven de 18 años conoce a una chica llamada Rize Kamishiro en una cafetería y la invita a salir. Tras una cita aparentemente normal, Kaneki se ofrece a acompañarla a su casa. Al entrar a un callejón es atacado por ella, quien resulta ser un Ghoul. Durante el ataque, Kaneki es salvado por la caída de unas vigas que matan a Rize antes de que pueda matarlo, pero lo deja terriblemente herido. Debido a la gravedad de sus heridas, recibe un trasplante de órganos por el ghoul atacante (Rize Kamishiro). Así, Kaneki termina convirtiéndose en un ser mitad Ghoul y mitad Humano, de ahora en adelante deberá vivir escondiéndose de los humanos sin tener a quién recurrir. Conoce a unos Ghouls quienes trabajan en la cafetería donde conoció a Rize y se entera que los trabajadores de esa cafetería también resultan ser ghouls que sobreviven a base de cafe, ya que el cafe es el único gusto que comparten los Ghouls y los humanos. Lo demás es rechazado por el estómago de los Ghouls, salvo la carne humana, que han de consumir cada cierto tiempo.Durante el transcurso de la historia Kaneki conocerá a nuevos compañeros y verá el lado oscuro de tanto la humanidad como de los Ghouls"));
+        listaAnimes.add(new Anime(1,"Ataque a los Titanes", false, R.drawable.ataque_a_los_titanes, "La historia comienza con Eren Jaeger quien vive de forma pacífica con su familia, su hermana adoptiva Mikasa Ackerman, y su único amigo Armin Arlert. Estos viven en el «distrito de Shiganshina» (シガンシナ区 Shiganshina-ku?), una de las principales ciudades, ubicadas en la muralla María. Pero todo cambia cuando un titán gigantesco crea una enorme grieta permitiendo la entrada de los titanes a la ciudad, mientras que otro titan fuertemente blindado traspasa la muralla María haciendo de su protección inútil, y provocando una evacuación masiva en toda la población hacia la muralla interna Rose, la segunda de tres murallas concéntricas. Durante el ataque, Eren observa con horror como un titán se come a su madre, mientras que su padre desaparece después de entregarle a Eren la llave del sótano de su casa, no sin antes encomendarle regresar a su hogar algún día. Después de la desaparición de su padre, Eren jura matar a todos los titanes y se alista en el ejército junto con sus amigos.\n" +
                 "\n" +
                 "Cinco años después, los tres jóvenes se han graduados de cadetes y viven en ese momento en el «distrito de Trost» (トロスト区 Torosuto-ku?), una de las ciudades fronterizas que sobresale de la muralla Rose. Sin embargo, apareció de nuevo el mismo titán y la historia se repite. En la batalla subsiguiente, Eren logra salvar a Armin de ser comido por un titán a costa de ser devorado. Mientras toda esperanza de sobrevivir parecía extinguirse, un titán musculoso y atípico aparece atacando a los otros titanes en lugar de a los humanos. Dicho titán se revela como Eren quien por alguna razón se ha transformado en uno, para sorpresa de todos, en especial de Mikasa y Armin. Aunque algunos, como los nobles, el clero, y la Policía militar, lo consideran una amenaza a pesar de ayudar a recuperar el Distrito de Trost, otros ven en la habilidad de Eren una oportunidad de salvar a la humanidad; por lo que después de ser sometido a un juicio militar en el que finalmente, se decidió que Eren forme parte del Cuerpo de exploración, asignándolo al Escuadrón de operaciones especiales bajo mando del capitán Levi.\n" +
                 "\n" +
                 "Conforme avanza la trama, se dan a conocer muchos misterios respecto a la capacidad de Eren en transformarse en titán, desde las razones de como consiguió dicho poder, el hecho de que no es el único que posee esta habilidad. También se descubrirán la procedencia y origen de los titanes, la revelación de sus verdaderos enemigos, y de las decisiones que Eren deberá tomar respecto al futuro de la humanidad, el de sus amigos, y el suyo propio. "));
+    }
+
+    /** Metodo onClick del menu **/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_anadir:
+                abreCrearAnime();
+                return true;
+            case R.id.favorito:
+               // addFavoritos(item);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     /** Abre la actividad del perfil de un item del RecyclerView */
@@ -136,6 +151,13 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
                 startActivityForResult(intencion, 101);
             }
         });
+    }
+
+    /** Abre la actividad del perfil de un item del RecyclerView */
+    public void abreCrearAnime(){
+        Intent intencion = new Intent(MainActivity.this, AddAnime.class);
+        intencion.putExtra("id", listaAnimes.size());
+        startActivityForResult(intencion, 102);
     }
 
     /** Guardar informacion de las otras actividades **/
